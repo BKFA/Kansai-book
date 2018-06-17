6859bb23278a454da99d52f12958ea08abb74a66
Toan <assassin.assassin7@gmail.com>
2018-06-17 16:56:52 +0700

    Add More FrontEnd Form CUD Admin

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,31 @@ Route::group(['prefix'=>'admin'],function(){
 		Route::post('update/{idtopic}','topicController@postUpdate');
 		Route::get('delete/{idtopic}','topicController@getDelete');
 	});
+
+	Route::group(['prefix'=>'post'],function(){
+		Route::get('list','postController@getList');
+		Route::get('create','postController@getCreate');
+		Route::post('create','postController@postCreate');
+		Route::get('update/{idtopic}','postController@getUpdate');
+		Route::post('update/{idtopic}','postController@postUpdate');
+		Route::get('delete/{idtopic}','postController@getDelete');
+	});
+
+	Route::group(['prefix'=>'tag'],function(){
+		Route::get('list','tagController@getList');
+		Route::get('create','tagController@getCreate');
+		Route::post('create','tagController@postCreate');
+		Route::get('update/{idtag}','tagController@getUpdate');
+		Route::post('update/{idtag}','tagController@postUpdate');
+		Route::get('delete/{idtag}','tagController@getDelete');
+	});
+
+	Route::group(['prefix'=>'user'],function(){
+		Route::get('list','userController@getList');
+		Route::get('create','userController@getCreate');
+		Route::post('create','userController@postCreate');
+		Route::get('update/{iduser}','userController@getUpdate');
+		Route::post('update/{iduser}','userController@postUpdate');
+		Route::get('delete/{iduser}','userController@getDelete');
+	});
 });
